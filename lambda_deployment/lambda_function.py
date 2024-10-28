215321b38dd0c9b2a4df786f1c9e78472f52d79b
EC2 Default User <ec2-user@ip-172-31-44-250.ec2.internal>
2024-10-28 17:09:53 +0000

    successful install of dd library

--- lambda_deployment/lambda_function.py
+++ lambda_deployment/lambda_function.py
@@ -1,10 +1,14 @@
 import json
-from degree_day import DegreeDayCalculator
+from degree_day import degree_day as dd
 
 def lambda_handler(event, context):
     
-    print(f'The lower temperature threshold is: {event["temp_low"]}')
-    print(f'The upper temperature threshold is: {event["temp_high"]}')
+    lt = event['temp_low']
+    ut = event['temp_high']
+    print(f'The lower temperature threshold is: {lt}')
+    print(f'The upper temperature threshold is: {ut}')
+
+    dd_calc = dd.DegreeDayCalculator(lower_threshold=lt, upper_threshold=ut)
 
     return {
         'statusCode': 200,
